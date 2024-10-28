0109001643b6fe15a16f56e02a1ab5e970e7863f
Paula Canuto <a24178@alunos.ipca.pt>
2024-10-28 21:47:11 +0000

    Primeiro Commit

---
+++ Music.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+class Music
+{
+    public string name;
+    public string artist;
+    public int time;
+    public bool avabble;
+    public void ShowTechnicalInfomation(){
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Artist: {artist}");
+        Console.WriteLine($"Time: {time} seconds");
+        if (avabble)
+        {
+            Console.WriteLine($"You can listen this song.");
+        }
+        else { 
+            Console.WriteLine($"Add Plus+ pack right now!"); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Music music1 = new Music();
+music1.name = "Everybody";
+music1.artist = "Backstreet Boys";
+music1.time = 203;
+music1.avabble = true;
+
+Music music2 = new Music();
+music2.name = "Don't Speak";
+music2.artist = "No Doubt";
+music2.time = 196;
+music2.avabble = true;
+
+Music music3 = new Music();
+music3.name = "All I Have To Give";
+music3.artist = "Backstreet Boys";
+music3.time = 216;
+music3.avabble = true;
+
+Music music4 = new Music();
+music4.name = "In The End";
+music4.artist = "Linkin Park";
+music4.time = 197;
+music4.avabble = true;
+
+Music music5 = new Music();
